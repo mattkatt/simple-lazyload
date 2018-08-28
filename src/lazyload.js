0ba0aa0d0b7d3772d7ab49c868754e4908b455d1
mattkatt <m4ttk4tt@hotmail.co.uk>
2018-08-28 11:57:01 +0100

    Create lazyload.js
    Initial commit of lazyload.js

---
+++ src/lazyload.js
@@ -0,0 +1,41 @@
+// LazyLoad
+// written by Matt Evans @mattkatt
+
+function lazyLoad() {
+  'use strict';
+  var i, targets = document.querySelectorAll(".lazyload");
+
+  if (targets.length > 0) {
+    for (i = 0; i < targets.length; i++) {
+      var elem = targets[i],
+          list = elem.classList,
+          src = elem.dataset.src,
+          rect = elem.getBoundingClientRect();
+
+      if (rect.top <= (window.innerHeight + 100)) {
+
+        if (list.contains("lazyBG")) {
+          elem.style.backgroundImage = "url(" + src + ")";
+        }
+
+        if (!list.contains("lazyBG")) {
+          elem.src = src;
+          if (elem.tagName === "VIDEO") {
+            elem.play();
+          }
+        }
+
+        list.remove("lazyload");
+      }
+    }
+  }
+
+  if (targets.length <= 0) {
+    window.removeEventListener('orientationchange', lazyLoad, false);
+    document.removeEventListener('scroll', lazyLoad, false);
+  }
+}
+
+window.addEventListener('load', lazyLoad, false);
+window.addEventListener('orientationchange', lazyLoad, false);
+document.addEventListener('scroll', lazyLoad, false);
