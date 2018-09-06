fee275ca4321bc20c96cddf404f0afd8062041ac
Matt Evans <m4ttk4tt@hotmail.co.uk>
2018-09-06 17:18:48 +0100

    Switched dataset for getAttribute
    this allows the code to work with IE10

--- src/lazyload.js
+++ src/lazyload.js
@@ -9,7 +9,7 @@ function lazyLoad() {
     for (i = 0; i < targets.length; i++) {
       var elem = targets[i],
           list = elem.classList,
-          src = elem.dataset.src,
+          src = elem.getAtrribute('data-src'),
           rect = elem.getBoundingClientRect();
 
       if (rect.top <= (window.innerHeight + 100) && elem.offsetParent !== null) {
