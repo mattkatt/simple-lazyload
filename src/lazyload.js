b303231abbb95be957439948f06e4f6842a857bb
Matt Evans <m4ttk4tt@hotmail.co.uk>
2018-10-09 13:01:20 +0100

    Support for IE9

--- src/lazyload.js
+++ src/lazyload.js
@@ -2,40 +2,40 @@
 // written by Matt Evans @mattkatt
 
 function lazyLoad() {
-  'use strict';
-  var i, targets = document.querySelectorAll(".lazyload");
+    'use strict';
+    var targets = document.querySelectorAll(".lazyload");
 
-  if (targets.length > 0) {
-    for (i = 0; i < targets.length; i++) {
-      var elem = targets[i],
-          list = elem.classList,
-          src = elem.getAtrribute('data-src'),
-          rect = elem.getBoundingClientRect();
+    if (targets.length > 0) {
+        for (i = 0; i < targets.length; i++) {
+            var elem = targets[i],
+            list = elem.getAttribute('class').split(' '),
+            lazyBG = list.indexOf("lazyBG"),
+            src = elem.getAttribute('data-src'),
+            rect = elem.getBoundingClientRect();
 
-      if (rect.top <= (window.innerHeight + 100) && elem.offsetParent !== null) {
+            if (rect.top <= (window.innerHeight + 200) && elem.offsetParent !== null) {
+                if (lazyBG > -1) {
+                    elem.style.backgroundImage = "url(" + src + ")";
+                }
 
-        if (list.contains("lazyBG")) {
-          elem.style.backgroundImage = "url(" + src + ")";
+                if (lazyBG <= -1) {
+                    elem.src = src;
+                    if (elem.tagName === "VIDEO") {
+                        elem.play();
+                    }
+                }
+
+                elem.setAttribute('class', list.splice(list.indexOf("lazyload"), 1));
+            }
         }
-
-        if (!list.contains("lazyBG")) {
-          elem.src = src;
-          if (elem.tagName === "VIDEO") {
-            elem.play();
-          }
-        }
-
-        list.remove("lazyload");
-      }
     }
-  }
 
-  if (targets.length <= 0) {
-    window.removeEventListener('orientationchange', lazyLoad, false);
-    document.removeEventListener('scroll', lazyLoad, false);
-  }
+    if (targets.length <= 0) {
+        window.removeEventListener('orientationchange', lazyLoad, false);
+        document.removeEventListener('scroll', lazyLoad, false);
+    }
 }
 
 window.addEventListener('load', lazyLoad, false);
 window.addEventListener('orientationchange', lazyLoad, false);
-document.addEventListener('scroll', lazyLoad, false)
+document.addEventListener('scroll', lazyLoad, false);
